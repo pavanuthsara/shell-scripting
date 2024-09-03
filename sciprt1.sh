3b53acc305ec1de4010dfae92afc4e6909b796d1
Pavan Uthsara <uthsarapavan@gmail.com>
2024-09-03 11:07:36 +0530

    v1

---
+++ sciprt1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo 'Enter -1 to exit'
+
+#loop begins
+while true
+do 
+	echo -n "Enter a number " 
+	read num
+
+	#check whether user enters -1
+	if test $num -eq -1
+	then 
+		break #break the loop if user enters -1
+	fi
+
+	if test $num -gt 0 
+	then 
+		echo "$num Positive value"
+	elif test $num -eq 0
+	then
+		echo "$num is zero" 
+	else
+		echo "$num Negative value"
+	fi
+
+done #end of while loop
+
+
